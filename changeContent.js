4800a9536dcc8393e31191dff81e9022ec5ac941
nogira <83847986+nogira@users.noreply.github.com>
2021-06-14 17:30:40 +1000

    Add files via upload

--- changeContent.js
+++ changeContent.js
@@ -14,8 +14,38 @@ function changeContent(filePath) {
   var content = loadFile(filePath);
 
   // markdown -> html
+  var content_array = content.split('\n');
   var html = '';
-  html +='';
+  var inCodeBlock = false;
+
+  for (i=0; i < content_array.length; i++) {
+    if (startsWith('\`\`\`')) {
+      if (content_array[i].length == 3) {
+        html += '<pre><code class="language-none">';
+      } else {
+        html += '<pre><code class="language-' + content_array[i].substring(3) + '">';
+      }
+    }
+  }
+
+
+
+//  for (i=0; i < content.length; i++) {
+    // grab first 4 characters at position i
+//    if (content.substr(i, 4) == '\n```'){
+      
+      // find space after
+//      for (j=0; j < content.length; j++) {
+//        if (content[i+j] = ' ') {
+        
+//        }
+//      }
+//html += '\n<code>'
+//    } else {
+//      html += content[i]
+//    }
+//  }
+  
 
   // replace text
   document.getElementById("content").innerHTML = content;
